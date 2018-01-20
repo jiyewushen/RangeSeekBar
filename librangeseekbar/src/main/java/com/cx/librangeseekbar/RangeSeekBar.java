b37d2cbe063a4a4df2207e1bb2a54fa6af2992bb
chen <1308370202@qq.com>
2018-01-20 18:38:10 +0800

    update

--- librangeseekbar/src/main/java/com/cx/librangeseekbar/RangeSeekBar.java
+++ librangeseekbar/src/main/java/com/cx/librangeseekbar/RangeSeekBar.java
@@ -226,5 +226,35 @@ public class RangeSeekBar extends ConstraintLayout {
         mSet.connect(view.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM);
         set.applyTo(this);
     }
-
+    public void addRightView(View view,int widthType,int heightType) {
+        rightView = view;
+        view.setId(View.generateViewId());
+        this.addView(view);
+        ConstraintSet set = new ConstraintSet();
+        set.clone(this);
+        mSet.constrainWidth(view.getId(),widthType);
+        mSet.constrainHeight(view.getId(),heightType);
+        mSet.connect(view.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
+        mSet.connect(view.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM);
+        mSet.connect(view.getId(), ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END);
+        set.applyTo(this);
+    }
+    public void addProgressView(View view,int widthType) {
+        addProgressView(view,widthType,ConstraintSet.MATCH_CONSTRAINT);
+    }
+    public void addProgressView(View view,int widthType,int heightType) {
+        progressView = view;
+        view.setId(View.generateViewId());
+        this.addView(view);
+        ConstraintSet set = new ConstraintSet();
+        int id=view.getId();
+        set.clone(this);
+        mSet.constrainWidth(id,widthType);
+        mSet.constrainHeight(id,heightType);
+        mSet.connect(id, ConstraintSet.START, leftView.getId(), ConstraintSet.END);
+        mSet.connect(id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
+        mSet.connect(id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM);
+        mSet.connect(id, ConstraintSet.END, rightView.getId(), ConstraintSet.START);
+        set.applyTo(this);
+    }
 }
